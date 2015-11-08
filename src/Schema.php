d47929d6f4d52944cbe156a53602295edebade20
clement <maboiteaspam@wanadoo.fr>
2015-11-08 14:04:53 +0800

    (init) Make it so!

--- src/Schema.php
+++ src/Schema.php
@@ -4,8 +4,22 @@ namespace C\Eloquent\Schema;
 use C\Schema\ISchema;
 use Illuminate\Database\Capsule\Manager as Capsule;
 
+/**
+ * Class EloquentSchema
+ * provide eloquent schema setup capability
+ *
+ * @package C\Eloquent\Schema
+ */
 abstract class EloquentSchema implements ISchema{
+
+    /**
+     * @var Capsule
+     */
     public $capsule;
+
+    /**
+     * @param Capsule $capsule
+     */
     public function __construct(Capsule $capsule) {
         $this->capsule = $capsule;
     }
